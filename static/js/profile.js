33700f61679b265a4022d842ea048e6977ac49ff
mkiterian <hmukite@gmail.com>
2017-07-28 00:38:46 +0300

    [Feature #148476585] Make radio buttons active only when checkbox is selected

--- static/js/profile.js
+++ static/js/profile.js
@@ -10,4 +10,15 @@ $(function() {
         return false;
     });
 
+    var $radios = $('input[name="period"]');
+    var $want_reports = $('input[name="reports_allowed"]');
+
+    $radios.prop("disabled", true)
+    $want_reports.on('click', function(){
+        if ( $(this).is(':checked') )
+            $radios.prop("disabled", false);
+        else
+            $radios.prop("disabled", true);
+    });
+
 });
